b1a452f21b65aa064733cb18674ade8a5a65d4d5
Hamid Azim <hamidazim321@gmail.com>
2023-11-20 15:37:51 +0500

    Populate animals table

--- data.sql
+++ data.sql
@@ -1,1 +1,6 @@
 /* Populate database with sample data. */
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES
+(' Charmander', '2022-02-08', 10.23, TRUE, 0),
+('Gabumon', '2018-11-15', 8.0, TRUE, 2),
+('Pikachu', '2021-01-07', 15.04, FALSE, 1),
+('Devimon', '2017-05-12', 11.0, TRUE, 5);
