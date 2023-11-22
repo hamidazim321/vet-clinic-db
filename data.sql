6d7c25326402aaacd209f14716a93f9528f0060b
Hamid Azim <hamidazim321@gmail.com>
2023-11-22 21:39:17 +0500

    Update data

--- data.sql
+++ data.sql
@@ -1,6 +1,6 @@
 /* Populate database with sample data. */
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES
-(' Charmander', '2022-02-08', 10.23, TRUE, 0),
+('Agumon', '2020-02-03', 10.23, TRUE, 0),
 ('Gabumon', '2018-11-15', 8.0, TRUE, 2),
 ('Pikachu', '2021-01-07', 15.04, FALSE, 1),
 ('Devimon', '2017-05-12', 11.0, TRUE, 5);
@@ -13,3 +13,34 @@ INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) 
 ('Boarmon', '2005-06-07', 20.4, TRUE, 7),
 ('Blossom', '1998-10-13', 17.0, TRUE, 3),
 ('Ditto', '2022-05-14', 22.0, TRUE, 4);
+
+INSERT INTO owners (fullname, age) VALUES
+('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
+
+INSERT INTO species (name) VALUES
+('Pokemon'),
+('Digimon')
+
+UPDATE animals
+SET species_id = 2
+WHERE name LIKE '%mon';
+
+UPDATE animals
+SET species_id = 1
+WHERE species_id IS NULL;
+
+UPDATE animals
+SET owner_id = 
+    CASE 
+        WHEN name = 'Agumon' THEN 1
+        WHEN name IN ('Gabumon', 'Pikachu') THEN 2
+        WHEN name IN ('Devimon', 'Plantmon') THEN 3
+        WHEN name IN ('Charmander', 'Squirtle', 'Blossom') THEN 4
+        WHEN name IN ('Angemon', 'Boarmon') THEN 5
+    END;
+
