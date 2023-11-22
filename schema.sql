51b7523be726919a18299a56239bf130d98a6457
Hamid Azim <hamidazim321@gmail.com>
2023-11-22 20:54:25 +0500

    Update schema

--- schema.sql
+++ schema.sql
@@ -13,3 +13,31 @@ CREATE TABLE animals (
 
 ALTER TABLE animals ADD
 species VARCHAR(255);
+
+CREATE TABLE owners (
+	id INT GENERATED ALWAYS AS IDENTITY,
+	fullname VARCHAR(255),
+	age INT,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE species (
+	id INT GENERATED ALWAYS AS IDENTITY,
+	name VARCHAR(255),
+	PRIMARY KEY (id)
+);
+
+ALTER TABLE animals
+DROP COLUMN species,
+ADD COLUMN species_id INT,
+ADD PRIMARY KEY (id),
+ADD CONSTRAINT fk_species
+    FOREIGN KEY (species_id)
+    REFERENCES species (id)
+    ON DELETE CASCADE,
+ADD COLUMN owner_id INT,
+ADD CONSTRAINT fk_owners
+	FOREIGN KEY (owner_id)
+	REFERENCES owners (id)
+	ON DELETE CASCADE;
+
